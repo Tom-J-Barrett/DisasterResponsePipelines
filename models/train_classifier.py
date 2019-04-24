b5be9876782f450774c49698587a42601e70717a
Tom Barrett <motb141@gmail.com>
2019-04-24 18:24:11 +0000

    Add train_classifier.py to train a classifier
    Signed-off-by: Tom Barrett <motb141@gmail.com>

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,76 @@
 import sys
+# import libraries
+import pandas as pd
+import re
+import numpy as np
+import pickle
+
+import nltk
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+
+from sqlalchemy import create_engine
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql("SELECT * FROM messages", engine)
+    X = df['message'] 
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis = 1)
+    
+    return X, Y, Y.columns
 
 
 def tokenize(text):
-    pass
+    text = text.lower() 
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text) 
+    
+    words = word_tokenize(text)
+    words = [w for w in words if w not in stopwords.words("english")]
+    
+    stemmed = [PorterStemmer().stem(w) for w in words]
+    
+    lemmed = [WordNetLemmatizer().lemmatize(w) for w in words]
+    
+    return lemmed
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', RandomForestClassifier())
+    ])
+    
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+
+    accuracy = (y_pred == Y_test).mean()
+    print("Accuracy:\n", accuracy)
 
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 
 def main():
